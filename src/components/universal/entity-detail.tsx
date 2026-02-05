a1d9edc3c3710af26ef2823d2c914436ef279063
Ted Slesinski <ted@slesinski.org>
2026-02-05 17:16:40 -0500

    Revert "fix: legacy SectionCard grid layout regression"
    This reverts commit c4eb25473427ef1243a216a813501b200ca9da4b.

--- src/components/universal/entity-detail.tsx
+++ src/components/universal/entity-detail.tsx
@@ -492,7 +492,7 @@ function SectionCard<T>({
         <CardTitle>{section.title}</CardTitle>
       </CardHeader>
       <CardContent>
-        <dl className="grid grid-cols-12 gap-4">
+        <dl className="grid grid-cols-2 gap-4">
           {section.fields?.map((field) => {
             const value = data[field.field as keyof T];
             // Check if this field is the state field and format using entity config
